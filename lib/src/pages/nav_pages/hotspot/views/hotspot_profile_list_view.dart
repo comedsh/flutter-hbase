bd6fd036b06713ef9e5156841f82fb4e54f90468
comedsh <comedshang@163.com>
2025-08-23 09:55:07 +0800

    follow impl in profile list view page

--- lib/src/pages/nav_pages/hotspot/views/hotspot_profile_list_view.dart
+++ lib/src/pages/nav_pages/hotspot/views/hotspot_profile_list_view.dart
@@ -53,13 +53,13 @@ class _HotspotProfileListViewState extends State<HotspotProfileListView> {
             padding: EdgeInsets.symmetric(vertical: sp(7), horizontal: sp(22)),
             child: Row(
               children: [
-                // 头像
+                /// 头像
                 GestureDetector(
                   onTap: () => Get.to(() => ProfilePage(profile: profile)),
                   child: ProfileAvatar(profile: profile, size: sp(66))
                 ),
                 SizedBox(width: sp(8)),
-                // 名字和描述
+                /// 名字和描述
                 GestureDetector(
                   onTap: () => Get.to(() => ProfilePage(profile: profile)),
                   child: SizedBox(
@@ -79,23 +79,41 @@ class _HotspotProfileListViewState extends State<HotspotProfileListView> {
                   ),
                 ),
                 SizedBox(width: sp(8.0)),
-                GradientElevatedButton(
-                  gradient: LinearGradient(colors: [
-                    AppServiceManager.appConfig.appTheme.fillGradientStartColor,
-                    AppServiceManager.appConfig.appTheme.fillGradientEndColor
-                  ]),
-                  width: sp(80),
-                  height: sp(32.0),
-                  borderRadius: BorderRadius.circular(13.0),
-                  onPressed: () {
-                  },
-                  dense: true,
-                  child: Text('关注', style: TextStyle(
-                    color: Colors.white, 
-                    fontSize: sp(14), 
-                    fontWeight: FontWeight.bold
-                  ))
-                ),
+                /// follow button
+                StatefulFollowButton(
+                  profile: profile, 
+                  followButtonCreator: ({required bool loading, required onTap}) => 
+                    GradientElevatedButton(
+                      gradient: LinearGradient(colors: [
+                        AppServiceManager.appConfig.appTheme.fillGradientEndColor,
+                        AppServiceManager.appConfig.appTheme.fillGradientEndColor
+                      ]),
+                      width: sp(80),
+                      height: sp(32.0),
+                      borderRadius: BorderRadius.circular(13.0),
+                      onPressed: () => onTap(context),
+                      dense: true,
+                      child: loading
+                      ? SizedBox(width: sp(14), height: sp(14), child: const CircularProgressIndicator(strokeWidth: 1.0, color: Colors.white))
+                      : Text('关注', style: TextStyle(color: Colors.white, fontSize: sp(14), fontWeight: FontWeight.bold))
+                    ),
+                  cancelFollowButtonCreator: ({required bool loading, required onTap}) => 
+                    TextButton(
+                      onPressed: () => onTap(context), 
+                      style: TextButton.styleFrom(
+                        /// 注意，下面三个参数是用来设置 TextButton 的内部 padding 的，默认的值比较大
+                        /// 参考 https://stackoverflow.com/questions/66291836/flutter-textbutton-remove-padding-and-inner-padding
+                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                        padding: EdgeInsets.zero,
+                        minimumSize: Size(sp(80), sp(32)),  // 重要：定义按钮的大小
+                        /// 设置 text button 的 border                          
+                        backgroundColor: Colors.black12.withOpacity(0.1)
+                      ),
+                      child: loading 
+                        ? SizedBox(width: sp(14), height: sp(14), child: const CircularProgressIndicator(strokeWidth: 1.5, color: Colors.white54))
+                        : Text('已关注', style: TextStyle(fontSize: sp(14), color: Colors.white54)),
+                    ),                  
+                )
               ],
             ),
           )
