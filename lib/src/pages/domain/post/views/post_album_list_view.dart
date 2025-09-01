fcd9fcbf0e955296f9999d2a2e806fed389e87b4
comedsh <comedshang@163.com>
2025-09-01 12:38:59 +0800

    try blur album

--- lib/src/pages/domain/post/views/post_album_list_view.dart
+++ lib/src/pages/domain/post/views/post_album_list_view.dart
@@ -155,15 +155,13 @@ class _PostAlbumListViewState extends State<PostAlbumListView> {
   ///      导致图片的长宽被压缩只有文本大小而导致布局的问题，建议是加上 width/height.
   Widget getCell(Post post) {
     final width = Math.round(Screen.width(context) / 3, fractionDigits: 2);
-
+    // 首先构建 badgeIcons
     var badgeIcons = <BadgeIcon>[];
-
     if (post.isPinned == true) {
       badgeIcons.add(BadgeIcon(
         location: BadgeIconLocation.leftTop, 
         icon: const Icon(Icons.push_pin, color: Colors.white, size: 26)));
     }
-
     if (post.type == PostType.album) {
       badgeIcons.add(BadgeIcon(
         location: BadgeIconLocation.rightTop, 
@@ -175,12 +173,13 @@ class _PostAlbumListViewState extends State<PostAlbumListView> {
         location: BadgeIconLocation.rightTop, 
         icon: const Icon(IconFont.icon_sy_video, color: Colors.white)));
     }
-
-    if (badgeIcons.isNotEmpty) {
-      return BadgedImage(imgUrl: post.thumbnail, width: width, badgeIcons: badgeIcons);
-    }
-    
-    return CachedImage(imgUrl: post.thumbnail, width: width);
+    // 然后创建 img
+    dynamic img = CachedImage(imgUrl: post.thumbnail, width: width);
+    img = post.blur == BlurType.blur 
+      ? BlurrableImage(img: img, unlockable: false, blurDepth: BlurDepth.deep,) 
+      : img;    
+    if (badgeIcons.isNotEmpty) img = BadgedImage(img: img, badgeIcons: badgeIcons);
+    return img;
   }
 
   scrollTo(index) {
