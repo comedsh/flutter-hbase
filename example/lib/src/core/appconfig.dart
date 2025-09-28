3dc58d9e2169db9add7a4ba6c66274bf234757a8
comedsh <comedshang@163.com>
2025-09-28 17:17:39 +0800

    remove redudant imports from example prj

--- example/lib/src/core/appconfig.dart
+++ example/lib/src/core/appconfig.dart
@@ -1,5 +1,7 @@
 
 
+// ignore_for_file: depend_on_referenced_packages
+
 import 'package:appbase/appbase.dart';
 import 'package:flutter/material.dart';
 import 'package:hbase/hbase.dart';
--- example/lib/src/core/initializer.dart
+++ example/lib/src/core/initializer.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: depend_on_referenced_packages
+
 import 'package:appbase/appbase.dart';
 import 'package:get/get.dart';
 import 'package:hbase/hbase.dart';
